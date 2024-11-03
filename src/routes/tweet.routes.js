51e22a2d293a08a263cc5e3ce7681635f4ca6350
Mohit Jangid <82498513+mohitjangir17@users.noreply.github.com>
2024-11-03 17:02:56 +0530

    Update tweet.routes.js

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -11,7 +11,7 @@ import { verifyJwt } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.post('/create', verifyJwt, createTweet)
-router.get('/get', getUserTweets)
+router.get('/get', verifyJwt, getUserTweets)
 router.get('/get-all', verifyJwt, getAllTweets)
 router.patch('/:tweetId/u', verifyJwt, updateTweet)
 router.delete('/:tweetId/d', verifyJwt, deleteTweet)
